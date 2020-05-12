7f8ba8b1dff1cad9810bce14d7dbab63aa0a2dc6
wgroeneveld <wouter.groeneveld@gmail.com>
2020-05-12 18:37:59 +0200

    fix sandwich from deleted js

--- themes/desem-swift-theme/assets/js/redzuurdesem.js
+++ themes/desem-swift-theme/assets/js/redzuurdesem.js
@@ -1,4 +1,70 @@
 (function() {
+
+ function modifyClass(el, targetClass) {
+    if (isObj(el) && targetClass) {
+      elClass = el.classList;
+      elClass.contains(targetClass) ? elClass.remove(targetClass) : elClass.add(targetClass);
+    }
+  }
+
+  function containsClass(el, targetClass) {
+    if (isObj(el) && targetClass && el !== document ) {
+      return el.classList.contains(targetClass) ? true : false;
+    }
+  }
+
+ function isObj(obj) {
+    return (obj && typeof obj === 'object' && obj !== null) ? true : false;
+  }
+
+ function elem(selector, parent = document){
+    let elem = parent.querySelector(selector);
+    return elem != false ? elem : false;
+  }
+
+  function elems(selector, parent = document) {
+    let elems = parent.querySelectorAll(selector);
+    return elems.length ? elems : false;
+  }
+
+	// from swift theme: sandwich
+	// ******
+	 (function() {
+	    let bar = 'nav_bar-wrap';
+	    let navBar = elem(`.${bar}`);
+	    let nav = elem('.nav-body');
+	    let open = 'nav-open';
+	    let exit = 'nav-exit';
+	    let drop = 'nav-drop';
+	    let pop = 'nav-pop';
+	    let navDrop = elem(`.${drop}`);
+
+	    function toggleMenu(){
+	      let menuOpen, menuPulled, status;
+	      modifyClass(navDrop, pop);
+	      modifyClass(navBar, 'hidden');
+	      menuOpen = containsClass(nav, open);
+	      menuPulled = containsClass(nav, exit);
+
+	      status = menuOpen || menuPulled ? true : false;
+
+	      status ? modifyClass(nav, exit) : modifyClass(nav, open);
+	      status ? modifyClass(nav, open) : modifyClass(nav, exit);
+	    }
+
+	    navBar.addEventListener('click', function() {
+	      toggleMenu();
+	    });
+	    elem('.nav-close').addEventListener('click', function() {
+	      toggleMenu();
+	    });
+
+	    elem('.nav-drop').addEventListener('click', function(e) {
+	      e.target === this ? toggleMenu() : false;
+	    });
+
+	  })();	
+
 	var fbposts = document.querySelector('#fbposts');
 	function whoops(error) {
 		console.log(error);
